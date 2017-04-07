3af22570177b9e7bdd138c0c027af2bfad205991
Junjun Zhang <junjun.ca@gmail.com>
2017-04-07 00:56:39 -0400

    added host ID, IP, worker ID etc

--- jtracker/jtracker.py
+++ jtracker/jtracker.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+import socket
 from .workflow import Workflow
 from .task_tracker import TaskTracker
 from .gitracker import GiTracker
@@ -5,7 +8,11 @@ from .utils import JOB_STATE
 
 
 class JTracker(object):
-    def __init__(self, git_repo_url=None, workflow_name=None, workflow_version=None):
+    def __init__(self, git_repo_url=None, workflow_name=None, workflow_version=None, jt_home=None):
+        self._init_jt_home(jt_home)
+        self._init_host_id()
+        self._init_host_ip()
+
         self._gitracker = GiTracker(git_repo_url, workflow_name=workflow_name, workflow_version=workflow_version)
 
         # TODO: generate JobTrackers from parse self.gitracker.workflow
@@ -18,6 +25,21 @@ class JTracker(object):
                         )
 
         self._build_dag()
+
+
+    @property
+    def jt_home(self):
+        return self._jt_home
+
+
+    @property
+    def host_id(self):
+        return self._host_id
+
+
+    @property
+    def host_ip(self):
+        return self._host_ip
 
 
     @property
@@ -37,6 +59,28 @@ class JTracker(object):
 
     def next_task(self, worker=None, timeout=None):
         pass
+
+
+    def _init_jt_home(self, jt_home):
+        if not jt_home: jt_home = os.environ['HOME']
+        self._jt_home = os.path.join(jt_home, '.jtracker')
+
+        if not os.path.isdir(self.jt_home):
+            os.makedirs(self.jt_home)
+
+
+    def _init_host_id(self):
+        host_id_file = os.path.join(self.jt_home, 'host_id')
+        if os.path.isfile(host_id_file):
+            with open(host_id_file, 'r') as f:
+                self._host_id = f.read().rstrip()
+        else:
+            with open(host_id_file, 'w') as f:
+                self._host_id = str(uuid.uuid4())[:8]
+                f.write(self.host_id)
+
+    def _init_host_ip(self):
+        self._host_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 
     def _build_dag(self):
--- jtracker/worker.py
+++ jtracker/worker.py
@@ -1,4 +1,6 @@
+import os
 import uuid
+
 
 class Worker(object):
     def __init__(self, jtracker=None, host_ip=None, host_name=None, cpu_cores=None, memory=None):
@@ -7,16 +9,27 @@ class Worker(object):
         self._host_name = host_name
         self._cpu_cores = cpu_cores
         self._memory = memory
-        self._worker_id = uuid.uuid4()
+        self._worker_id = '.'.join(['worker', str(uuid.uuid4()),
+                                    'host', self.jtracker.host_id, self.jtracker.host_ip])
+
         self._current_task = None
+
+        self._record_local_git_path()  # more of for debugging purpose
 
 
     @property
     def jtracker(self):
         return self._jtracker
 
+
+    @property
+    def worker_id(self):
+        return self._worker_id
+
+
     def current_task(self):
         return self._current_task
+
 
     def next_task(self):
         if self._current_task: return  # if current task exists, return None
@@ -28,17 +41,27 @@ class Worker(object):
         else:
             return
 
+
     def log_task_info(self, status):
         self.current_task.log_task_info(self.current_task, info={})  # info must be dict
 
+
     def task_json(self):
         return self.current_task.task_json(self.current_task.task_file)
+
 
     def task_failed(self):
         self.current_task.task_failed(self.current_task.task_file)
         self._current_task = None
 
+
     def task_completed(self):
         self.current_task.task_completed(self.current_task.task_file)
         self._current_task = None
+
+
+    def _record_local_git_path(self):
+        worker_id_file = os.path.join(self.jtracker.jt_home, self.worker_id)
+        with open(worker_id_file, 'w')as f:
+            f.write('%s\n' % self.jtracker.gitracker.local_git_path)
 
